89f58dd7832b1c25f47f94f868fbc8e2be42c13a
riyanmgeorge <110010573+riyanmgeorge@users.noreply.github.com>
2022-08-17 09:45:51 +0530

    Initial Commit

--- Main.py
+++ Main.py
@@ -2,3 +2,6 @@
 length_of_circular_linked_list = int(input())
 # Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
+actual_list = []
+
+value = 0
